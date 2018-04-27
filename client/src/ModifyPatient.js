465a071414aa478805bce9d7adc1ad59887707d8
Giang Nguyen <giang.ng.1994@gmail.com>
2018-04-27 19:14:19 -0400

    40 bucks

--- client/src/ModifyPatient.js
+++ client/src/ModifyPatient.js
@@ -103,21 +103,32 @@ class ModifyPatient extends React.Component {
   }
 
   renderEditable(cellInfo) {
-    return (
-      <div
-        style={{ backgroundColor: "#fafafa" }}
-        contentEditable
-        suppressContentEditableWarning
-        onBlur={e => {
-          const data = [...this.state.data];
-          data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
-          this.setState({ data });
-        }}
-        dangerouslySetInnerHTML={{
-          __html: this.state.data[cellInfo.index][cellInfo.column.id]
-        }}
-      />
-    );
+    if (cellInfo.index==0){
+      return (
+          <div 
+          dangerouslySetInnerHTML={{
+              __html: this.state.data[cellInfo.index][cellInfo.column.id]
+            }}
+          />
+      );
+    }
+    else{
+      return (
+        <div
+          style={{ backgroundColor: "#fafafa" }}
+          contentEditable
+          suppressContentEditableWarning
+          onBlur={e => {
+            const data = [...this.state.data];
+            data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+            this.setState({ data });
+          }}
+          dangerouslySetInnerHTML={{
+            __html: this.state.data[cellInfo.index][cellInfo.column.id]
+          }}
+        />
+      );
+    }
   }
 
 
