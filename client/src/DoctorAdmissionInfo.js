280e27d0816dbccf0285e7553465c828969bd755
Giang Nguyen <giang.ng.1994@gmail.com>
2018-04-28 22:42:32 -0400

    Doctor a bit

--- client/src/DoctorAdmissionInfo.js
+++ client/src/DoctorAdmissionInfo.js
@@ -4,6 +4,8 @@ import { render } from "react-dom";
 // Import React Table
 import ReactTable from "react-table";
 import "react-table/react-table.css";
+import AddNewAdmission from "./AddNewAdmission";
+import ModifyReport from "./ModifyReport";
 import { makeDataPerson, makeDataAdmission, Logo, Tips } from "./Utils";
 import matchSorter from 'match-sorter'
 
@@ -11,7 +13,9 @@ import matchSorter from 'match-sorter'
 class DoctorAdmissionInfo extends React.Component {
   constructor() {
     super();
+    this.handleChangePageClick=this.handleChangePageClick.bind(this);
     this.state = {
+      Page: 0,
       admissionData: [],
       EnterTime1: '',
       EnterTime2: '',
@@ -35,35 +39,59 @@ class DoctorAdmissionInfo extends React.Component {
 
   }
 
+  handleChangePageClick(num){
+    this.setState({Page:num});
+  }
 
   componentDidMount(){
     console.log('Component has mounted');
 
-    fetch("/api/admission")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          //console.log(result[0]);
-          let arr = [];
-          for (var i = 0; i< result.length; i++){
-            arr.push(result[i]);
+    if (this.props.singleID){
+      fetch("api/admission/"+this.props.singleID)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            //console.log(result[0]);
+            let arr = [];
+            for (var i = 0; i< result.length; i++){
+              arr.push(result[i]);
 
+            }
+
+            this.setState({admissionData: arr});
+            console.log(this.state);
           }
+        )
 
-          this.setState({admissionData: arr});
-          //console.log(this.state);
-        }
+    }
+    else {
+      fetch("/api/admission")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            //console.log(result[0]);
+            let arr = [];
+            for (var i = 0; i< result.length; i++){
+              arr.push(result[i]);
 
-    )
+            }
+
+            this.setState({admissionData: arr});
+            //console.log(this.state);
+          }
+        )
+    }
   }
-
 
 
   render() {
 
+    let page = [];
+
+
     let constdata  = this.state.admissionData;
 
-    let rangeCond = testcase(constdata, this.state.EnterTime1, this.state.EnterTime2, 'EnterTime');
+    let rangeCond = testcase(constdata, this.state.EnterTime1, this.state.EnterTime2, 'enter_time');
 
     if(rangeCond !== -1){
       var admissionData = rangeCond;
@@ -72,7 +100,7 @@ class DoctorAdmissionInfo extends React.Component {
       var admissionData = constdata;
     }
 
-    rangeCond = testcase(admissionData, this.state.LeaveTime1, this.state.LeaveTime2, 'LeaveTime');
+    rangeCond = testcase(admissionData, this.state.LeaveTime1, this.state.LeaveTime2, 'leave_time');
 
     if(rangeCond !== -1){
       admissionData = rangeCond;
@@ -81,117 +109,131 @@ class DoctorAdmissionInfo extends React.Component {
 
     }
 
+
+
+    switch(this.state.Page){
+      case 0:
+        page.push(
+          <div>
+
+          <ReactTable
+            getTdProps={(state, rowInfo, column, instance) => {
+              return {
+                onClick: (e, handleOriginal) => {
+                  console.log("A Td Element was clicked!");
+                  console.log("it produced this event:", e);
+                  console.log("It was in this column:", column);
+                  console.log("It was in this row:", rowInfo);
+                  console.log("It was in this table instance:", instance);
+
+                  // IMPORTANT! React-Table uses onClick internally to trigger
+                  // events like expanding SubComponents and pivots.
+                  // By default a custom 'onClick' handler will override this functionality.
+                  // If you want to fire the original onClick handler, call the
+                  // 'handleOriginal' function.
+                  if (handleOriginal) {
+                    
+
+                  }
+                }
+              };
+            }}
+            data={admissionData}
+            filterable
+            columns={[
+
+
+              {
+                Header: "AID",
+                accessor: "aid",
+                filterMethod: (filter, rows) =>
+                      matchSorter(rows, filter.value, { keys: ["aid"] }),
+                filterAll: true,
+                width: 50
+                },
+              {
+                Header: "Name",
+                accessor: "name",
+                filterMethod: (filter, rows) =>
+                      matchSorter(rows, filter.value, { keys: ["name"] }),
+                filterAll: true,
+                width: 100
+              },
+              {
+                Header: "Enter Time",
+                accessor: "enter_time",
+
+                filterMethod: (filter, rows) =>
+                     matchSorter(rows, filter.value, { keys: ["enter_time"] }),
+                filterAll: true,
+                Filter: () => (
+                  <div >
+                    <form action="/action_page.php">
+                     <input id="date" type="date" size="1" onChange={(event) => this.handleChange(event.target.value, 0)}>
+                   </input>-
+                     <input id="date" type="date" size="1" onChange={(event) => this.handleChange(event.target.value, 1)}></input>
+                    </form>
+                  </div>),
+                width: 100
+              },
+              {
+                Header: "Leave Time",
+                accessor: "leave_time",
+                filterMethod: (filter, rows) =>
+                     matchSorter(rows, filter.value, { keys: ["leave_time"] }),
+                     Filter: () => (
+                       <div >
+                         <form action="/action_page.php">
+                         <input id="date" type="date" size="1" onChange={(event) => this.handleChange(event.target.value, 2)}>
+                     </input>-
+                         <input id="date" type="date" size="1" onChange={(event) => this.handleChange(event.target.value, 3)}></input>
+                         </form>
+                       </div>),
+
+                width: 100
+              },
+              {
+                Header: "Payment",
+                accessor: "paymentinfo",
+                filterMethod: (filter, rows) =>
+                      matchSorter(rows, filter.value, { keys: ["paymentinfo"] }),
+                filterAll: true,
+                width: 100
+              },
+              {
+                Header: "Insurance",
+                accessor: "insurancecover",
+                filterMethod: (filter, rows) =>
+                      matchSorter(rows, filter.value, { keys: ["insurancecover"] }),
+                filterAll: true,
+                width: 100
+              },
+              {
+                Header: "Notes",
+                accessor: "detail",
+                filterMethod: (filter, rows) =>
+                      matchSorter(rows, filter.value, { keys: ["detail"] }),
+                filterAll: true,
+                width: 100
+              },
+
+
+              
+
+            ]}
+
+            defaultPageSize={10}
+            className="-striped -highlight"
+          />
+          <br />
+
+        </div>
+        );
+        break;
+      }
     return (
       <div>
-
-        <ReactTable
-          getTdProps={(state, rowInfo, column, instance) => {
-            return {
-              onClick: (e, handleOriginal) => {
-                console.log("A Td Element was clicked!");
-                console.log("it produced this event:", e);
-                console.log("It was in this column:", column);
-                console.log("It was in this row:", rowInfo);
-                console.log("It was in this table instance:", instance);
-
-                // IMPORTANT! React-Table uses onClick internally to trigger
-                // events like expanding SubComponents and pivots.
-                // By default a custom 'onClick' handler will override this functionality.
-                // If you want to fire the original onClick handler, call the
-                // 'handleOriginal' function.
-                if (handleOriginal) {
-                  handleOriginal();
-                }
-              }
-            };
-          }}
-          data={admissionData}
-          filterable
-          columns={[
-
-
-            {
-              Header: "AID",
-              accessor: "aid",
-              filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["AID"] }),
-              filterAll: true,
-              width: 50
-              },
-            {
-              Header: "Name",
-              accessor: "name",
-              filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["name"] }),
-              filterAll: true,
-              width: 100
-            },
-            {
-              Header: "Enter Time",
-              accessor: "enter_time",
-
-              filterMethod: (filter, rows) =>
-                   matchSorter(rows, filter.value, { keys: ["EnterTime"] }),
-                   Filter: () => (
-                     <div >
-                     <form action="/action_page.php">
-                     <input type="search"  name="search" size="1" onChange={(event) => this.handleChange(event.target.value, 0)}
-                     ></input>-
-                     <input type="search"  name="search" size="1" onChange={(event) => this.handleChange(event.target.value, 1)}></input>
-                     </form>
-                     </div>),
-              filterAll: true,
-
-              width: 100
-            },
-            {
-              Header: "Leave Time",
-              accessor: "leave_time",
-              filterMethod: (filter, rows) =>
-                   matchSorter(rows, filter.value, { keys: ["LeaveTime"] }),
-                   Filter: () => (
-                     <div >
-                     <form action="/action_page.php">
-                     <input type="search"  name="search" size="1" onChange={(event) => this.handleChange(event.target.value, 2)}
-                     ></input>-
-                     <input type="search"  name="search" size="1" onChange={(event) => this.handleChange(event.target.value, 3)}></input>
-                     </form>
-                     </div>),
-
-              width: 100
-            },
-            {
-              Header: "Payment",
-              accessor: "paymentinfo",
-              filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["PaymentInfo"] }),
-              filterAll: true,
-              width: 100
-            },
-            {
-              Header: "Insurance",
-              accessor: "insurancecover",
-              filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["InsuranceCover"] }),
-              filterAll: true,
-              width: 100
-            },
-            {
-              Header: "Notes",
-              accessor: "detail",
-              filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, { keys: ["Detail"] }),
-              filterAll: true,
-
-            }
-
-          ]}
-
-          defaultPageSize={10}
-          className="-striped -highlight"
-        />
-        <br />
-
+      {page}
       </div>
     );
   }
@@ -202,7 +244,6 @@ export default DoctorAdmissionInfo
 function addDays(theDate, days) {
     return new Date(theDate.getTime() + days*24*60*60*1000);
 }
-
 
 function testcase(rows, val1, val2, key){
   let dataLength = rows.length;
--- client/src/DoctorClientInfo.js
+++ client/src/DoctorClientInfo.js
@@ -2,30 +2,47 @@ import React from "react";
 import { render } from "react-dom";
 
 // Import React Table
+
+import ModifyPatient from './ModifyPatient';
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import { makeDataPerson, Logo, Tips } from "./Utils";
 import matchSorter from 'match-sorter'
+import BookRoom from './BookRoom';
+import ModifyAdmission from './ModifyAdmission';
+import AddNewPatient from './AddNewPatient';
+import AdminAdmissionInfo from './AdminAdmissionInfo';
+import DoctorAdmissionInfo from './DoctorAdmissionInfo';
+import AddNewAdmission from './AddNewAdmission';
+import AdminReportInfo from './AdminReportInfo';
 
 
 class DoctorClientInfo extends React.Component {
-  //constructor
   constructor() {
     super();
+    this.handleChangePageClick = this.handleChangePageClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
+      Page: 0,
+      /*
+      Modify Patient 1
+      Add New Patient 2
+      Delete 3
+      History Report 4
+      Leave Room 5
+      Book Room 6
+      History Admission 7
+      
+      */
+      singlePatientID: [],
+      singlePatientName: [],
+      singlePatientGender: [],
+      singlePatientSSN: [],
       patientData: []
     };
   }
 
-  handleChange(e, num){
-    if(num == 0){
-      this.setState({ AgeRange1: e });
-    }else{
-      this.setState({ AgeRange2: e });
 
-    }
-  }
 
   //API
   componentDidMount(){
@@ -50,9 +67,27 @@ class DoctorClientInfo extends React.Component {
     )
   }
 
+
+  handleChangePageClick(num){
+    this.setState({Page:num});
+  }
+
+  handleChange(e, num){
+    if(num == 0){
+      this.setState({ AgeRange1: e });
+    }else{
+      this.setState({ AgeRange2: e });
+
+    }
+  }
+
+
   render() {
     const { patientData } = this.state;
-
+    const { singlePatientID } = this.state;
+    const { singlePatientName } = this.state;
+    const {singlePatientGender} = this.state;
+    const {singlePatientSSN} = this.state;
     let rangeCond = getRange(patientData, this.state.AgeRange1, this.state.AgeRange2, 'age');
     if(rangeCond !== -1){
       var data = rangeCond;
@@ -60,44 +95,62 @@ class DoctorClientInfo extends React.Component {
     }else{
       var data = patientData;
     }
+    //console.log('toReact',{patientData});
+    let page = [];
+    const onRowClick = (state, rowInfo, column, instance) => {
+      return {
+          onClick: (e, handleOriginal) => {
+              console.log('A Td Element was clicked!');
+              console.log('it produced this event:', e);
+              console.log('It was in this column:', column);
+              console.log('It was in this row:', rowInfo);
+              console.log('It was in this table instance:', instance);
+              this.setState({singlePatientID:rowInfo["original"]["pid"]});
+              this.setState({singlePatientName:rowInfo["original"]["name"]});
+              this.setState({singlePatientGender:rowInfo["original"]["gender"]});
+              this.setState({singlePatientSSN:rowInfo["original"]["pssn"]});
 
-    return (
-      <div>
 
-
-        <ReactTable
-          getTdProps={(state, rowInfo, column, instance) => {
-            return {
-              onClick: (e, handleOriginal) => {
-                console.log("A Td Element was clicked!");
-                console.log("it produced this event:", e);
-                console.log("It was in this column:", column);
-                console.log("It was in this row:", rowInfo);
-                console.log("It was in this table instance:", instance);
-
-                // IMPORTANT! React-Table uses onClick internally to trigger
-                // events like expanding SubComponents and pivots.
-                // By default a custom 'onClick' handler will override this functionality.
-                // If you want to fire the original onClick handler, call the
-                // 'handleOriginal' function.
-                if (handleOriginal) {
-                  handleOriginal();
+              if (handleOriginal) {
+                
+               
+                
+                if (column["Header"] == "History"){
+                  if (column.parentColumn.Header == "Admission"){
+                    this.setState({Page:7});
+                  }
+                  if (column.parentColumn.Header == "Report"){
+                    this.setState({Page:4});
+                  }
                 }
+                
+
               }
-            };
-          }}
-          data={patientData}
-          filterable
-          columns={[
-            {
+          }
+      }
+    }
+
+    switch(this.state.Page){
+      case 0:
+        page.push(
+          <div>
+          <div>
+          <button type="button" onClick={this.handleChangePageClick.bind(this,2)}> Add New Patient </button>
+          </div>
+            <ReactTable
+              getTdProps={onRowClick}
+              data={data}
+              filterable
+              columns={[
+                {
                   Header: "Details",
                   filterable: false,
                   columns: [
-                  {
+                    {
                       Header: "PID",
                       accessor: "pid",
                       filterMethod: (filter, rows) =>
-                            matchSorter(rows, filter.value, { keys: ["PID"] }),
+                            matchSorter(rows, filter.value, { keys: ["pid"] }),
                       filterAll: true,
                       width: 50
                       },
@@ -107,10 +160,10 @@ class DoctorClientInfo extends React.Component {
                       filterMethod: (filter, rows) =>
                             matchSorter(rows, filter.value, { keys: ["name"] }),
                       filterAll: true,
-
+                      width: 100
                     },
                     {
-                      Header: "Sex",
+                      Header: "Gender",
                       accessor: "gender",
                       id: "sex",
                       //filterMethod: (filter, rows) =>
@@ -137,23 +190,29 @@ class DoctorClientInfo extends React.Component {
                           <option value="false">Female</option>
                         </select>,
 
-                      width: 45
+                      width: 70
                     },
                     {
                       Header: "Age",
                       accessor: "age",
-                      filterMethod: (filter, rows) =>
-                           matchSorter(rows, filter.value, { keys: ["age"] }),
-                           Filter: () => (
-                                    <div >
-                                      <form action="/action_page.php">
-                                        <input type="search"  name="search" size="1" onChange={(event) => this.handleChange(event.target.value, 0)}
-                                     ></input>-
-                                       <input type="search"  name="search" size="1" onChange={(event) => this.handleChange(event.target.value, 1)}></input>
-                                      </form>
-                                    </div>),
+                      Filter: () => (
+                               <div >
+                                 <form action="/action_page.php">
+                                   <input type="search"  name="search" size="1" onChange={(event) => this.handleChange(event.target.value, 0)}
+                                ></input>-
+                                  <input type="search"  name="search" size="1" onChange={(event) => this.handleChange(event.target.value, 1)}></input>
+                                 </form>
+                               </div>),
 
-                      width: 50
+                      width: 100
+                    },
+                    {
+                      Header: "SSN",
+                      accessor: "pssn",
+                      filterMethod: (filter, rows) =>
+                            matchSorter(rows, filter.value, { keys: ["pssn"] }),
+                      filterAll: true,
+                      width: 100
                     }
 
                   ]
@@ -169,22 +228,7 @@ class DoctorClientInfo extends React.Component {
                       accessor: "rid",
                       width: 50
                     },
-
-                    {
-                      Header: "Book",
-                      //accessor: "age"
-                      filterable: false,
-
-                      Cell: row => (
-                        <div
-                          style={{
-                            width: "100%",
-                            height: "100%",
-                            backgroundColor: "coral",
-                            borderRadius: "2px"
-                          }}
-                        > Book </div>   )
-                    }
+                    
                   ]
 
                 },
@@ -196,17 +240,21 @@ class DoctorClientInfo extends React.Component {
                       Header: "History",
                       //accessor: "age"
                       filterable: false,
-
+                      width: 75,
                       Cell: row => (
                         <div
                           style={{
                             width: "100%",
                             height: "100%",
-                            backgroundColor: "coral",
+                            fontStyle: "italic",
+                            color: "#0066ff",
+                            textDecoration: "underline",
+                            textAlign: "center",
                             borderRadius: "2px"
                           }}
                         > History </div>   )
-                    }
+                    },
+                    
                   ]
                 },
                 {
@@ -218,29 +266,83 @@ class DoctorClientInfo extends React.Component {
                       Header: "History",
                       //accessor: "age"
                       filterable: false,
-
+                      width: 75,
                       Cell: row => (
                         <div
                           style={{
                             width: "100%",
                             height: "100%",
-                            backgroundColor: "coral",
+                            fontStyle: "italic",
+                            color: "#0066ff",
+                            textDecoration: "underline",
+                            textAlign: "center",
                             borderRadius: "2px"
                           }}
                         > History </div>   )
+                    },
+                    {
+                      Header: "New",
+                      //accessor: "age"
+                      filterable: false,
+                      width: 75,
+                      Cell: row => (
+                        <div
+                          style={{
+                            width: "100%",
+                            height: "100%",
+                            fontStyle: "italic",
+                            color: "#0066ff",
+                            textDecoration: "underline",
+                            textAlign: "center",
+                            borderRadius: "2px"
+                          }}
+                        > New </div>   )
                     }
 
+
                   ]
-                }
+                },
+                
+              ]}
+
+              defaultPageSize={10}
+              className="-striped -highlight"
+            />
+            <br />
+
+          </div>
+          );
+        break;
+
+      case 1:
+        page.push(<ModifyPatient singlePatientFromParent={singlePatientID}/>);
+        break;
+      case 2:
+        page.push(<AddNewPatient/>);
+        break;
+      case 4:
+        page.push(<DoctorAdmissionInfo singleID={singlePatientID}/>);
+        break;
+      case 6:
+        page.push(<BookRoom singleID={singlePatientID} singleGender={singlePatientGender} singleName={singlePatientName} singleSSN={singlePatientSSN}/>);
+        break;
+      case 7:
+        page.push(<AdminAdmissionInfo singleID={singlePatientID}/>);
+        break;
+      case 8:
+        page.push(<AddNewAdmission singleID={singlePatientID} singleName={singlePatientName}/>);
+        break;
+    //    case 6:
+      //    page.push(<BookRoom/>);
+        //  break;
+
+      }
 
 
+    return (
+      <div className="DoctorClientInfo">
 
-          ]}
-
-          defaultPageSize={10}
-          className="-striped -highlight"
-        />
-        <br />
+          {page}
 
       </div>
     );
@@ -249,38 +351,39 @@ class DoctorClientInfo extends React.Component {
 
 export default DoctorClientInfo
 
-function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-function getRange(rows, val1, val2, key){
-  let dataLength = rows.length;
-  let data = [];
-  let value1 = parseInt(val1,10);
-  let value2 = parseInt(val2,10);
-  if(isNumber(val1) && val1 != ''){
+//helper function
+  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+  function getRange(rows, val1, val2, key){
+    let dataLength = rows.length;
+    let data = [];
+    let value1 = parseInt(val1,10);
+    let value2 = parseInt(val2,10);
+    if(isNumber(val1) && val1 != ''){
 
 
-    for(let i = 0; i < dataLength; i++){
-      if(val2 != '' && isNumber(val2)){
-        if(rows[i][key] >= value1 && rows[i][key] <= value2){
-          data.push(rows[i]);
-        }
+      for(let i = 0; i < dataLength; i++){
+        if(val2 != '' && isNumber(val2)){
+          if(rows[i][key] >= value1 && rows[i][key] <= value2){
+            data.push(rows[i]);
+          }
 
-      } else {
+        } else {
 
-        if(rows[i][key] == value1){
-          data.push(rows[i]);
+          if(rows[i][key] == value1){
+            data.push(rows[i]);
+          }
+
         }
 
       }
 
+      return data;
+
     }
 
-    return data;
+    return -1;
+
 
   }
-
-  return -1;
-
-
-}
